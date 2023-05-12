f191a2bf07e74b7ea069cd6574de66d355f8f762
pachi81 <pachi81@web.de>
2023-05-12 12:28:12 +0200

    Add help menu item

--- mobile/src/main/java/de/michelinside/glucodatahandler/MainActivity.kt
+++ mobile/src/main/java/de/michelinside/glucodatahandler/MainActivity.kt
@@ -15,8 +15,12 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
-import de.michelinside.glucodatahandler.common.*
-import de.michelinside.glucodatahandler.common.notifier.*
+import de.michelinside.glucodatahandler.common.Constants
+import de.michelinside.glucodatahandler.common.ReceiveData
+import de.michelinside.glucodatahandler.common.WearPhoneConnection
+import de.michelinside.glucodatahandler.common.notifier.InternalNotifier
+import de.michelinside.glucodatahandler.common.notifier.NotifierInterface
+import de.michelinside.glucodatahandler.common.notifier.NotifyDataSource
 
 
 class MainActivity : AppCompatActivity(), NotifierInterface {
@@ -30,82 +34,107 @@ class MainActivity : AppCompatActivity(), NotifierInterface {
     private val LOG_ID = "GlucoDataHandler.Main"
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.d(LOG_ID, "onCreate called")
-        val intent = Intent()
-        val packageName = packageName
-        val pm = getSystemService(POWER_SERVICE) as PowerManager
-        if (!pm.isIgnoringBatteryOptimizations(packageName)) {
-            intent.action = Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
-            intent.data = Uri.parse("package:$packageName")
-            startActivity(intent)
-        }
-
-        GlucoDataServiceMobile.start(this)
-
-
-        txtBgValue = findViewById(R.id.txtBgValue)
-        viewIcon = findViewById(R.id.viewIcon)
-        txtLastValue = findViewById(R.id.txtLastValue)
-        txtWearInfo = findViewById(R.id.txtWearInfo)
-        txtCarInfo = findViewById(R.id.txtCarInfo)
-
-        PreferenceManager.setDefaultValues(this, R.xml.preferences, false)
-        sharedPref = this.getSharedPreferences(Constants.SHARED_PREF_TAG, Context.MODE_PRIVATE)
-
-        ReceiveData.initData(this)
-
-        txtVersion = findViewById(R.id.txtVersion)
-        txtVersion.text = BuildConfig.VERSION_NAME
-
-        val sendToAod = sharedPref.getBoolean(Constants.SHARED_PREF_SEND_TO_GLUCODATA_AOD, false)
-
-        if(!sharedPref.contains(Constants.SHARED_PREF_GLUCODATA_RECEIVERS)) {
-            val receivers = HashSet<String>()
-            if (sendToAod)
-                receivers.add("de.metalgearsonic.glucodata.aod")
-            Log.i(LOG_ID, "Upgrade receivers to " + receivers.toString())
-            with(sharedPref.edit()) {
-                putStringSet(Constants.SHARED_PREF_GLUCODATA_RECEIVERS, receivers)
-                apply()
+        try {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            Log.d(LOG_ID, "onCreate called")
+            val intent = Intent()
+            val packageName = packageName
+            val pm = getSystemService(POWER_SERVICE) as PowerManager
+            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+                intent.action = Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
+                intent.data = Uri.parse("package:$packageName")
+                startActivity(intent)
             }
+
+            GlucoDataServiceMobile.start(this)
+
+
+            txtBgValue = findViewById(R.id.txtBgValue)
+            viewIcon = findViewById(R.id.viewIcon)
+            txtLastValue = findViewById(R.id.txtLastValue)
+            txtWearInfo = findViewById(R.id.txtWearInfo)
+            txtCarInfo = findViewById(R.id.txtCarInfo)
+
+            PreferenceManager.setDefaultValues(this, R.xml.preferences, false)
+            sharedPref = this.getSharedPreferences(Constants.SHARED_PREF_TAG, Context.MODE_PRIVATE)
+
+            ReceiveData.initData(this)
+
+            txtVersion = findViewById(R.id.txtVersion)
+            txtVersion.text = BuildConfig.VERSION_NAME
+
+            val sendToAod = sharedPref.getBoolean(Constants.SHARED_PREF_SEND_TO_GLUCODATA_AOD, false)
+
+            if(!sharedPref.contains(Constants.SHARED_PREF_GLUCODATA_RECEIVERS)) {
+                val receivers = HashSet<String>()
+                if (sendToAod)
+                    receivers.add("de.metalgearsonic.glucodata.aod")
+                Log.i(LOG_ID, "Upgrade receivers to " + receivers.toString())
+                with(sharedPref.edit()) {
+                    putStringSet(Constants.SHARED_PREF_GLUCODATA_RECEIVERS, receivers)
+                    apply()
+                }
+            }
+        } catch (exc: Exception) {
+            Log.e(LOG_ID, "onCreate exception: " + exc.message.toString() )
         }
     }
 
     override fun onPause() {
-        super.onPause()
-        InternalNotifier.remNotifier(this)
-        Log.d(LOG_ID, "onPause called")
+        try {
+            super.onPause()
+            InternalNotifier.remNotifier(this)
+            Log.d(LOG_ID, "onPause called")
+        } catch (exc: Exception) {
+            Log.e(LOG_ID, "onPause exception: " + exc.message.toString() )
+        }
     }
 
     override fun onResume() {
-        super.onResume()
-        Log.d(LOG_ID, "onResume called")
-        update()
-        InternalNotifier.addNotifier(this, mutableSetOf(
-            NotifyDataSource.BROADCAST,
-            NotifyDataSource.MESSAGECLIENT,
-            NotifyDataSource.CAPILITY_INFO,
-            NotifyDataSource.NODE_BATTERY_LEVEL,
-            NotifyDataSource.SETTINGS,
-            NotifyDataSource.CAR_CONNECTION,
-            NotifyDataSource.OBSOLETE_VALUE))
+        try {
+            super.onResume()
+            Log.d(LOG_ID, "onResume called")
+            update()
+            InternalNotifier.addNotifier(this, mutableSetOf(
+                NotifyDataSource.BROADCAST,
+                NotifyDataSource.MESSAGECLIENT,
+                NotifyDataSource.CAPILITY_INFO,
+                NotifyDataSource.NODE_BATTERY_LEVEL,
+                NotifyDataSource.SETTINGS,
+                NotifyDataSource.CAR_CONNECTION,
+                NotifyDataSource.OBSOLETE_VALUE))
+        } catch (exc: Exception) {
+            Log.e(LOG_ID, "onResume exception: " + exc.message.toString() )
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        Log.d(LOG_ID, "onCreateOptionsMenu called")
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_items, menu)
+        try {
+            Log.d(LOG_ID, "onCreateOptionsMenu called")
+            val inflater = menuInflater
+            inflater.inflate(R.menu.menu_items, menu)
+            return true
+        } catch (exc: Exception) {
+            Log.e(LOG_ID, "onCreateOptionsMenu exception: " + exc.message.toString() )
+        }
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        Log.d(LOG_ID, "onOptionsItemSelected for " + item.itemId.toString())
-        if (item.itemId == R.id.action_settings) {
-            val intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
-            return true
+        try {
+            Log.d(LOG_ID, "onOptionsItemSelected for " + item.itemId.toString())
+            if (item.itemId == R.id.action_settings) {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                return true
+            } else if (item.itemId == R.id.action_help) {
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(resources.getText(R.string.help_link).toString()))
+                startActivity(browserIntent)
+                return true
+            }
+        } catch (exc: Exception) {
+            Log.e(LOG_ID, "onOptionsItemSelected exception: " + exc.message.toString() )
         }
         return super.onOptionsItemSelected(item)
     }
