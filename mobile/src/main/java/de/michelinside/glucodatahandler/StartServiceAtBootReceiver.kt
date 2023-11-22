f0304b874f3fb8b207f7541e3b43e397ce6ccef6
pachi81 <56817032+pachi81@users.noreply.github.com>
2023-11-22 16:20:40 +0100

    Update StartServiceAtBootReceiver.kt

--- mobile/src/main/java/de/michelinside/glucodatahandler/StartServiceAtBootReceiver.kt
+++ mobile/src/main/java/de/michelinside/glucodatahandler/StartServiceAtBootReceiver.kt
@@ -9,11 +9,13 @@ import de.michelinside.glucodatahandler.common.GlucoDataService
 
 class StartServiceAtBootReceiver: BroadcastReceiver() {
 
-    private val LOG_ID = "GlucoDataHandler.StartMyServiceAtBootReceiver"
-    override fun onReceive(context: Context, intent: Intent) {
-        if (Intent.ACTION_BOOT_COMPLETED == intent.action && !GlucoDataService.running) {
-            Log.d(LOG_ID, "Start Service after booting")
+    private val LOG_ID = "GlucoDataHandler.StartServiceAtBootReceiver"
+    override fun onReceive(context: Context, intent: Intent) {       
+        try {
+            Log.i(LOG_ID, "Start Service after intent action received: " + intent.action)
             GlucoDataServiceMobile.start(context, true)
+        } catch( exc: Exception ) {
+            Log.e(LOG_ID, exc.message + "\n" + exc.stackTraceToString())
         }
     }
 }
