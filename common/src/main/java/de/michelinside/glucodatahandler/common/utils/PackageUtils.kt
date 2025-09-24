a4fa9465348833a08c4c5891283418057cc15076
pachi81 <pachi81@web.de>
2025-09-24 19:15:23 +0200

    Add Medtrum/Signos to package filter

--- common/src/main/java/de/michelinside/glucodatahandler/common/utils/PackageUtils.kt
+++ common/src/main/java/de/michelinside/glucodatahandler/common/utils/PackageUtils.kt
@@ -163,6 +163,8 @@ object PackageUtils {
             receiverFilter.add("com.camdiab.")   // Cam APS FX
             receiverFilter.add("com.medtronic.")  // Medtronic MiniMed
             receiverFilter.add("com.microtech.aidexx.") // LinX CGM
+            receiverFilter.add("com.medtrum.")  // Medtrum
+            receiverFilter.add("com.signos.")  // Signos
         }
         return receiverFilter
     }
